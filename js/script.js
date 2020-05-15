aa558bdc9b2ad923a53f4fe7f397c53ad39ac2d3
Arsen Trynko <jarkodara@gmail.com>
2020-05-15 17:01:28 +0300

    Valodathion

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+function validate(){
+var name = document.getElementById("name");
+var phone = document.getElementById("phone");
+
+if(!name.value){
+	name.style.border = "2px solid red";
+	return false;
+   }
+
+   if(!phone.value){
+	phone.style.border = "2px solid red";
+	return false;
+   }
+
+return true;
+
+}
